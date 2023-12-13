c25a98570b9d6a3471294b6279e852700ed4499d
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2023-12-13 23:17:00 +0400

    Task 6 - Handle unsubscription

--- projects/forms-playground/src/app/core/dynamic-validation-errors/validation-error-tracker.directive.ts
+++ projects/forms-playground/src/app/core/dynamic-validation-errors/validation-error-tracker.directive.ts
@@ -1,16 +1,18 @@
-import { ComponentRef, Directive, OnInit, ViewContainerRef, inject } from '@angular/core';
+import { ComponentRef, Directive, OnDestroy, OnInit, ViewContainerRef, inject } from '@angular/core';
 import { FormControlName } from '@angular/forms';
 import { ErrorsListComponent } from './errors-list.component';
-import { startWith } from 'rxjs';
+import { Subscription, startWith } from 'rxjs';
 
 @Directive({
   selector: '[validationErrorTracker]',
   standalone: true,
 })
-export class ValidationErrorTrackerDirective implements OnInit {
+export class ValidationErrorTrackerDirective implements OnInit, OnDestroy {
   formControl = inject(FormControlName, {self: true});
   container = inject(ViewContainerRef);
   errorListRef: ComponentRef<ErrorsListComponent> | null = null;
+
+  #formControlStatus!: Subscription;
   /**
    * This directive is responsible for listening to the status changes (Valid/Invalid)
    * of the formControl which resides on the same node with this directive.
@@ -19,7 +21,7 @@ export class ValidationErrorTrackerDirective implements OnInit {
    * as an input for errors-list component. Otherwise, the component has to be destroyed.
    */
   ngOnInit() {
-    this.formControl.control.statusChanges.pipe(
+    this.#formControlStatus = this.formControl.control.statusChanges.pipe(
       startWith(this.formControl.control.status)
     ).subscribe((status) => {
       if (status === 'INVALID') {
@@ -31,5 +33,8 @@ export class ValidationErrorTrackerDirective implements OnInit {
         this.errorListRef = null;
       }
     })
+  }
+  ngOnDestroy() {
+    this.#formControlStatus.unsubscribe();
   }
 }
