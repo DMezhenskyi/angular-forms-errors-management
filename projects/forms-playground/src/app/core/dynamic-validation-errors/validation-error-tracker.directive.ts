f0c66da00c81916f5b3ec25064932431ba78311f
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2023-12-13 21:41:38 +0400

    Task 3 - create a subscription to status changes of the form control

--- projects/forms-playground/src/app/core/dynamic-validation-errors/validation-error-tracker.directive.ts
+++ projects/forms-playground/src/app/core/dynamic-validation-errors/validation-error-tracker.directive.ts
@@ -1,11 +1,11 @@
-import { Directive, inject } from '@angular/core';
+import { Directive, OnInit, inject } from '@angular/core';
 import { FormControlName } from '@angular/forms';
 
 @Directive({
   selector: '[validationErrorTracker]',
   standalone: true,
 })
-export class ValidationErrorTrackerDirective {
+export class ValidationErrorTrackerDirective implements OnInit {
   formControl = inject(FormControlName, {self: true});
   /**
    * This directive is responsible for listening to the status changes (Valid/Invalid)
@@ -14,4 +14,7 @@ export class ValidationErrorTrackerDirective {
    * the instance of the errors-list component and pass errors from the form control
    * as an input for errors-list component. Otherwise, the component has to be destroyed.
    */
+  ngOnInit() {
+    this.formControl.control.statusChanges.subscribe(console.log)
+  }
 }
